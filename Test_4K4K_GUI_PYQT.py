d68fcbf39d26ed6235a70cedb9ada95c28ecb659
iancat <papernyangi@gmail.com>
2024-10-29 09:14:41 +0900

    add 4K4K-GUI PYQT version

---
+++ Test_4K4K_GUI_PYQT.py
@@ -0,0 +1,121 @@
+import os.path
+import sys
+import ScreenReader
+import recolorImage
+from PyQt5.QtGui import QIcon, QFont, QPixmap, QPen, QPainter, QFontDatabase
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QPushButton,
+    QToolTip,
+    QMainWindow,
+    qApp,
+    QAction,
+    QLabel,
+    QVBoxLayout,
+    QTabWidget,
+    QGridLayout,
+    QLayout,
+    QHBoxLayout
+)
+
+colorSelect = 7
+menu_select = 0
+
+class AnotherWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.w = monitor1
+        self.initUI()
+
+    def initUI(self):
+        r = int(colorSelect / 4)
+        g = int(colorSelect / 2) - 2 * r
+        b = colorSelect - 4 * r - 2 * g
+        if menu_select == 0:
+            self.setLayout(self.image_Show())
+
+        self.move(self.w.xPos(1), self.w.yPos(1))
+        self.showFullScreen()
+    def image_Show(self):
+        r = int(colorSelect / 4)
+        g = int(colorSelect / 2) - 2 * r
+        b = colorSelect - 4 * r - 2 * g
+        pixmap = self.image_pixmap(r, g, b)
+        lbl_img = QLabel()
+        lbl_img.setPixmap(pixmap)
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_img)
+        vbox.setContentsMargins(0, 0, 0, 0)
+        return vbox
+    def image_pixmap(self, r, g, b):
+        pix = recolorImage.recolorImage(r, g, b, self.w.width(1), self.w.height(1))
+        return pix
+class MyApp(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.w1 = AnotherWindow()
+        self.initUI()
+    def show_new_window(self, checked):
+       self.w1.show()
+
+    def hide_new_window(self, checked):
+        self.w1.hide()
+    def create_tabs_Image(self):
+        button1 = QPushButton(' RED  ', self)
+        button1.move(50, 50)
+        button1.resize(100,10)
+        button1.clicked.connect(self.w1.image_Show)
+        button1.setStyleSheet("padding: 15px; background-color: red; color: white;")
+        # button1.setFont(QFont("Helvetica", 15))
+
+        button2 = QPushButton(' BLUE  ', self)
+        button2.move(80, 50)
+        button2.resize(100, 10)
+        button2.clicked.connect(self.w1.image_Show)
+        button2.setStyleSheet("padding: 15px; background-color: blue; color: white;")
+
+
+        button3 = QPushButton(' GREEN  ', self)
+        button3.move(100, 50)
+        button3.resize(100, 10)
+        button3.clicked.connect(self.w1.image_Show)
+        button3.setStyleSheet("padding: 15px; background-color: green; color: white;")
+
+        # btn = QPushButton('quit', self)
+        # btn.move(50, 50)
+        # btn.resize(btn.sizeHint())
+        # btn.clicked.connect(QCoreApplication.instance().quit)
+        hbox = QHBoxLayout()
+        hbox.addWidget(button1)
+        hbox.addWidget(button2)
+        hbox.addWidget(button3)
+        # tab = QWidget()
+        # tab.setLayout(hbox)
+        return hbox
+    def initUI(self):
+        self.setWindowTitle('4K4K Pattern Generator')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.setGeometry(200, 200, 800, 800)
+
+        # self.show()
+
+def ExceptionHook(exctype, value, traceback):
+    sys.__excepthook__(exctype, value, traceback)
+    sys.exit(1)
+
+if __name__ == '__main__':
+    sys.excepthook = ExceptionHook
+    monitor1 = ScreenReader.monitor
+    monitor1.scanning(monitor1)
+
+
+    font = QFont("SF Pro Compressed Medium", 16)
+    font1 = QFont("Times New Roman")
+    app = QApplication(sys.argv)
+    app.setFont(font)
+    ex = MyApp()
+    ex.show()
+    sys.exit(app.exec_())
